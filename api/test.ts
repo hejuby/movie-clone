ce35d1855b867763e8c4517d2491c77520101b87
Heejun Byeon <hejuby@gmail.com>
2024-03-14 23:48:40 +0900

    build: add .env file

--- api/test.ts
+++
@@ -1,7 +0,0 @@
-export default function handler(request, response) {
-  response.status(200).json({
-    name: 'hejuby',
-    age: 18,
-    isValid: true
-  })
-}
--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,3 @@ const root = document.querySelector('#root');
 root?.append(new App().el);
 
 router();
-
-(async () => {
-  const res = await fetch('/api/test');
-  console.log(res);
-  const json  = await res.json();
-  console.log('/api/test', json);
-})();
